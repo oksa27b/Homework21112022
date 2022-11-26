03bf6012e808c4cb4ea2fd08d9bce9a8a59e6abb
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-26 14:02:18 +0300

    /Seminar_4_task_27/Program.cs

---
+++ Seminar_4_task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Write number " );
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма " + GetSumNum(num));
+int GetSumNum(int input)
+{
+    int sum = 0;
+    while(input > 0)
+    {
+        sum += input % 10;
+        input = input/10;
+    }
+    return sum;
+}
