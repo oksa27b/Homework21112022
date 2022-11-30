dd7f9e2a0da5228326b2a6c6099d3c2f33299047
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-30 18:55:41 +0300

    /Seminar_6_task_41/Program.cs

---
+++ Seminar_6_task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Сколько чисел вы хотите ввести? ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [numb];
+
+int PositiveCount(int count)
+{
+    int countOfPositive = 0;
+    for(int i = 0; i < count;i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+        {
+            countOfPositive+=1;
+        }
+        Console.WriteLine(string.Join(",", array));
+    }
+    return countOfPositive;
+}
+int posCount = PositiveCount(numb);
+Console.Write($"количество положительных чисел = {posCount}");
