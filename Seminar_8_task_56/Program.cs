9da35ac8b068c2066d5a3030e48e2d8f9e617c6f
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-05 18:12:43 +0300

    /Seminar_8_task_56/Program.cs

---
+++ Seminar_8_task_56/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] array = new int[new Random().Next(2,7), new Random().Next(2,7)];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(0,12);
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+    
+Console.WriteLine("____________");
+
+int Sum = 0;
+int minSum = Int32.MaxValue;
+int minRow = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Sum += array[i,j];
+    }
+    if (Sum < minSum)
+    {
+        minSum = Sum;
+        minRow = i;
+    }
+    Sum = 0;
+}
+
+Console.WriteLine("Номер строки с наименьшей суммой элементов: " + minRow);
+
+
+
